6685f7ba8413c37ab435da6648737fa46a99f997
Ana Catarina Resende <anaresende93@gmail.com>
2023-11-29 20:58:14 +0000

    feat: add more whiteboard sync

--- src/app/whiteboard/whiteboard.component.ts
+++ src/app/whiteboard/whiteboard.component.ts
@@ -28,37 +28,66 @@ export class WhiteboardComponent implements OnInit, AfterViewInit {
   };
   private drawing: boolean = false;
   message: string = '';
-  syncClient: any;
+  syncClient: TwilioSync.Client | undefined;
+  syncStream: any;
 
   ngOnInit(): void {
-    this.initializeSyncClient();
+    this.getTokenAndInitializeSync();
   }
 
-  initializeSyncClient(): void {
+  getTokenAndInitializeSync(): void {
     axios
       .get('http://localhost:8050/whiteboard')
       .then((response: any) => {
-        this.syncClient = new TwilioSync.Client(response.data.token, {
-          logLevel: 'info',
-        });
-        this.syncClient.on('connectionStateChanged', (state: string) => {
-          if (state !== 'connected') {
-            this.message = `Sync is not live (websocket connection <span style="color: red">${state}</span>)...`;
-          } else {
-            this.message = 'Sync is live!';
-          }
-        });
+        const token = response.data.token;
+        this.initializeSync(token);
       })
       .catch((error: any) => {
         console.error('Error fetching token:', error);
       });
   }
 
+  initializeSync(token: string): void {
+    const syncClient = new TwilioSync.Client(token, {
+      logLevel: 'info',
+    });
+
+    syncClient.on('connectionStateChanged', (state: string) => {
+      if (state !== 'connected') {
+        this.message = `Sync is not live (websocket connection <span style="color: red">${state}</span>)...`;
+      } else {
+        this.message = 'Sync is live!';
+      }
+    });
+  }
+
+  createSyncStream(syncClient: TwilioSync.Client): void {
+    syncClient.stream('drawingData').then((stream) => {
+      stream.on('messagePublished', (event: any) => {
+        this.syncDrawingData(event.message.value);
+      });
+    });
+  }
+
+  syncDrawingData(data: any): void {
+    const canvas = this.canvasRef.nativeElement;
+    const context = canvas.getContext('2d');
+
+    const w = canvas.width;
+    const h = canvas.height;
+    this.drawLine(
+      data.x0 * w,
+      data.y0 * h,
+      data.x1 * w,
+      data.y1 * h,
+      data.color
+    );
+  }
+
   ngAfterViewInit(): void {
     const canvas: HTMLCanvasElement = this.canvasRef.nativeElement;
     this.context = canvas.getContext('2d')!;
     this.setupEventListeners();
-    this.onResize();
   }
 
   private drawLine(
@@ -68,54 +97,69 @@ export class WhiteboardComponent implements OnInit, AfterViewInit {
     y1: number,
     color: string
   ) {
-    this.context.beginPath();
-    this.context.moveTo(x0, y0);
-    this.context.lineTo(x1, y1);
-    this.context.strokeStyle = color;
-    this.context.lineWidth = 2;
-    this.context.stroke();
-    this.context.closePath();
+    const canvas = this.canvasRef.nativeElement;
+    const context = canvas.getContext('2d');
+
+    // Draw line on the canvas
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
+
+    // Publish drawing data to the Sync Stream if it exists
+    if (this.syncStream) {
+      const w = canvas.width;
+      const h = canvas.height;
+
+      this.syncStream.publishMessage({
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color: color,
+      });
+    }
   }
 
-  private onMouseDown(e: MouseEvent) {
-    this.drawing = true;
-    this.current.x = e.clientX;
-    this.current.y = e.clientY;
-  }
-
-  private onMouseUp(e: MouseEvent) {
+  onMouseUp(e: MouseEvent): void {
     if (!this.drawing) {
       return;
     }
     this.drawing = false;
+    const rect = this.canvasRef.nativeElement.getBoundingClientRect();
     this.drawLine(
       this.current.x,
       this.current.y,
-      e.clientX,
-      e.clientY,
+      e.clientX - rect.left,
+      e.clientY - rect.top,
       this.current.color
     );
   }
 
-  private onMouseMove(e: MouseEvent) {
+  private onMouseDown(e: MouseEvent): void {
+    this.drawing = true;
+    const rect = this.canvasRef.nativeElement.getBoundingClientRect();
+    this.current.x = e.clientX - rect.left;
+    this.current.y = e.clientY - rect.top;
+  }
+
+  onMouseMove(e: MouseEvent): void {
     if (!this.drawing) {
       return;
     }
+    const rect = this.canvasRef.nativeElement.getBoundingClientRect();
     this.drawLine(
       this.current.x,
       this.current.y,
-      e.clientX,
-      e.clientY,
+      e.clientX - rect.left,
+      e.clientY - rect.top,
       this.current.color
     );
-    this.current.x = e.clientX;
-    this.current.y = e.clientY;
-  }
-
-  private onResize() {
-    const canvas: HTMLCanvasElement = this.canvasRef.nativeElement;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    this.current.x = e.clientX - rect.left;
+    this.current.y = e.clientY - rect.top;
   }
 
   private setupEventListeners() {
@@ -124,12 +168,5 @@ export class WhiteboardComponent implements OnInit, AfterViewInit {
     canvas.addEventListener('mouseup', this.onMouseUp.bind(this));
     canvas.addEventListener('mouseout', this.onMouseUp.bind(this));
     canvas.addEventListener('mousemove', this.onMouseMove.bind(this));
-
-    window.addEventListener('resize', this.onResize.bind(this));
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onWindowResize(event: Event) {
-    this.onResize();
   }
 }
