483bf0b202fc0fd76cfcf172b9b60d5f39b01914
Ana Catarina Resende <anaresende93@gmail.com>
2023-11-29 19:15:16 +0000

    feat: add login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import axios from 'axios';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -18,4 +20,33 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginData = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private router: Router) {}
+
+  handleLoginSubmit() {
+    const credentials = {
+      email: this.loginData.email,
+      password: this.loginData.password,
+    };
+
+    axios
+      .post('localhost:8050/login', credentials)
+      .then((response) => {
+        this.router.navigate(['/subject-list']);
+        console.log('Login successful!', response.data);
+      })
+      .catch((error) => {
+        // Handle error response here
+        console.error('Login failed!', error);
+      });
+  }
+
+  handleRegisterClick() {
+    this.router.navigate(['/register']);
+  }
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
